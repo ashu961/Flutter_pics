72a5ec0fc470fd176d5fd36ef69ea313d2f48fc2
ashu961 <beheraashutosh5@gmail.com>
2019-08-24 10:10:57 +0530

    first commit

---
+++ lib/src/models/image_model.dart
@@ -0,0 +1,12 @@
+class ImageModel {
+  int id;
+  String title;
+  String url;
+
+  ImageModel(this.id, this.url, this.title);
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    url = parsedJson['url'];
+    title = parsedJson['title'];
+  }
+}
